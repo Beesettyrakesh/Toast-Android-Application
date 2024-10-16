ee7c83b838c2a78954ee05a8796567caca6de391
rakesh <rakeshbeesetty53@gmail.com>
2024-10-16 11:39:28 +0530

    Basic Toast Message

---
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -0,0 +1,28 @@
+package com.example.myapplication
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val btnUpload = findViewById<Button>(R.id.btnUpload)
+        val btnDownload = findViewById<Button>(R.id.btnDownload)
+
+        btnUpload.setOnClickListener {
+            Toast
+                .makeText(applicationContext, "Uploading...", Toast.LENGTH_SHORT)
+                .show()
+        }
+
+        btnDownload.setOnClickListener {
+            Toast
+                .makeText(applicationContext, "Downloading...", Toast.LENGTH_SHORT)
+                .show()
+        }
+    }
+}
